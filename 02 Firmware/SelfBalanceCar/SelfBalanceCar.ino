17e21878c0ffb42925530e68a1f437100110dc01
Jasongeng <1198894714@qq.com>
2023-04-24 16:26:24 +0800

    Change 0424

--- 02 Firmware/SelfBalanceCar/SelfBalanceCar.ino
+++ 02 Firmware/SelfBalanceCar/SelfBalanceCar.ino
@@ -9,7 +9,15 @@
   If you want to know what pin the on-board LED is connected to on your Arduino
   model, check the Technical Specs of your board at:
 */
-
+/*
+  Introduce Wire Repository
+*/
+#include <Wire.h>
+const int MPU = 0x68;//MPU IIC defult address
+float AccX, AccY, AccZ;
+float GyroX, GyroY, GyroZ;
+float accAngleX, accAngleY, accAngleZ;
+float roll;
 // the setup function runs once when you press reset or power the board
 //Declare Initial Variable
 #define SDA A4
@@ -35,8 +43,7 @@
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  //  TB6612
+  //Config TB6612 Pins
   pinMode(PWMA, OUTPUT);
   pinMode(PWMB, OUTPUT);
   pinMode(AIN1, OUTPUT);
@@ -45,20 +52,28 @@ void setup()
   pinMode(BIN2, OUTPUT);
   pinMode(STBY, OUTPUT);
   digitalWrite(STBY, HIGH);
-  //  pinMode(, OUTPUT);
-  //  pinMode(, OUTPUT);
-  //  pinMode(, OUTPUT);
-  //  pinMode(, OUTPUT);
-  //  pinMode(, OUTPUT);
-  //  pinMode(, OUTPUT);
+  //Call for MPU Init and communicate function
+  WireInit();
+  //
+
 }
 //Main Loop
 void loop()
 {
-  Forward();
-  digitalWrite(PWMA,100);
-  digitalWrite(PWMB,100);
-  delay(delayTime);
+  //  Forward();
+  //  digitalWrite(PWMA,100);
+  //  digitalWrite(PWMB,100);
+  //  delay(delayTime);
+}
+void WireInit()
+{
+  Serial.begin(9600);
+  Wire.begin();                      // Initialize comunication
+  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);                  // Talk to the register 6B
+  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
+  Wire.endTransmission(true);        //end the transmission
+  delay(20);
 }
 void Forward()
 {
